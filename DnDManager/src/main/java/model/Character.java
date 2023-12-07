d7a8f62be2e75d0b1c5afba883a25fe5502accb5
Gerardo Villanueva <a22216369@alumnos.uady.mx>
2023-12-06 22:53:50 -0600

    Implementando metodos de personaje

--- DnDManager/src/main/java/model/Character.java
+++ DnDManager/src/main/java/model/Character.java
@@ -165,6 +165,150 @@ public class Character {
 		builder.append("]");
 		return builder.toString();
 	}
+	public String getCharacterName() {
+		return characterName;
+	}
+	public void setCharacterName(String characterName) {
+		this.characterName = characterName;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public Race getRace() {
+		return race;
+	}
+	public void setRace(Race race) {
+		this.race = race;
+	}
+	public Myclass getCalss() {
+		return calss;
+	}
+	public void setCalss(Myclass calss) {
+		this.calss = calss;
+	}
+	public int getCurrentHP() {
+		return currentHP;
+	}
+	public void setCurrentHP(int currentHP) {
+		this.currentHP = currentHP;
+	}
+	public int getMaxHP() {
+		return maxHP;
+	}
+	public void setMaxHP(int maxHP) {
+		this.maxHP = maxHP;
+	}
+	public int getCharisma() {
+		return charisma;
+	}
+	public void setCharisma(int charisma) {
+		this.charisma = charisma;
+	}
+	public int getCharismaMod() {
+		return charismaMod;
+	}
+	public void setCharismaMod(int charismaMod) {
+		this.charismaMod = charismaMod;
+	}
+	public int getIntelligence() {
+		return intelligence;
+	}
+	public void setIntelligence(int intelligence) {
+		this.intelligence = intelligence;
+	}
+	public int getIntelligenceMod() {
+		return intelligenceMod;
+	}
+	public void setIntelligenceMod(int intelligenceMod) {
+		this.intelligenceMod = intelligenceMod;
+	}
+	public int getWisdom() {
+		return wisdom;
+	}
+	public void setWisdom(int wisdom) {
+		this.wisdom = wisdom;
+	}
+	public int getWisdomMod() {
+		return wisdomMod;
+	}
+	public void setWisdomMod(int wisdomMod) {
+		this.wisdomMod = wisdomMod;
+	}
+	public int getConstitution() {
+		return constitution;
+	}
+	public void setConstitution(int constitution) {
+		this.constitution = constitution;
+	}
+	public int getConstitutionMod() {
+		return constitutionMod;
+	}
+	public void setConstitutionMod(int constitutionMod) {
+		this.constitutionMod = constitutionMod;
+	}
+	public int getDexterity() {
+		return dexterity;
+	}
+	public void setDexterity(int dexterity) {
+		this.dexterity = dexterity;
+	}
+	public int getDexterityMod() {
+		return dexterityMod;
+	}
+	public void setDexterityMod(int dexterityMod) {
+		this.dexterityMod = dexterityMod;
+	}
+	public int getStrength() {
+		return strength;
+	}
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+	public int getStrengthMod() {
+		return strengthMod;
+	}
+	public void setStrengthMod(int strengthMod) {
+		this.strengthMod = strengthMod;
+	}
+	public Storage getStorage() {
+		return storage;
+	}
+	public void setStorage(Storage storage) {
+		this.storage = storage;
+	}
+	public Weapon getWeaponSlot() {
+		return weaponSlot;
+	}
+	public void setWeaponSlot(Weapon weaponSlot) {
+		this.weaponSlot = weaponSlot;
+	}
+	public Item getShieldSlot() {
+		return shieldSlot;
+	}
+	public void setShieldSlot(Item shieldSlot) {
+		this.shieldSlot = shieldSlot;
+	}
+	public Item getArmorSlot() {
+		return armorSlot;
+	}
+	public void setArmorSlot(Item armorSlot) {
+		this.armorSlot = armorSlot;
+	}
+	public int getMoney() {
+		return money;
+	}
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	public int getCA() {
+		return CA;
+	}
+	public void setCA(int cA) {
+		CA = cA;
+	}
 
 
     /* 
@@ -193,6 +337,8 @@ public class Character {
 
     }
     */
+	
+	
     
     
 }
